f2b71ca7538d904f9ea672b9d84ef11e21764f4f
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2017-11-08 15:16:54 -0500

    Add project files.

---
+++ SportsStore.Tests/NavigationMenuViewComponentTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Xunit;
+using Moq;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using SportsStore.Components;
+using SportsStore.Models;
+
+namespace SportsStore.Tests
+{
+    public class NavigationMenuViewComponentTests
+    {
+        [Fact]
+        public void Can_Select_Categories()
+        {
+            //  Arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1", Category = "Apples"},
+                new Product {ProductID = 2, Name = "P2", Category = "Apples"},
+                new Product {ProductID = 3, Name = "P3", Category = "Plums"},
+                new Product {ProductID = 4, Name = "P4", Category = "Oranges"}
+            });
+
+            NavigationMenuViewComponent target = new NavigationMenuViewComponent(mock.Object);
+
+            //  Act
+            string[] results = ((IEnumerable<string>)(target.Invoke() as ViewViewComponentResult).ViewData.Model).ToArray();
+
+            //  Assert
+            Assert.True(Enumerable.SequenceEqual(new string[] { "Apples", "Oranges", "Plums" }, results));
+        }
+
+        [Fact]
+        public void Indicates_Selected_Category()
+        {
+            //  Arrange
+            string categoryToSelect = "Apples";
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1", Category = "Apples"},
+                new Product { ProductID = 4, Name = "P2", Category = "Oranges"}
+            });
+
+            NavigationMenuViewComponent target = new NavigationMenuViewComponent(mock.Object);
+            target.ViewComponentContext = new ViewComponentContext
+            {
+                ViewContext = new Microsoft.AspNetCore.Mvc.Rendering.ViewContext { RouteData = new Microsoft.AspNetCore.Routing.RouteData() }
+            };
+            target.RouteData.Values["category"] = categoryToSelect;
+
+            //  Act
+            string result = (string)(target.Invoke() as ViewViewComponentResult).ViewData["SelectedCategory"];
+
+            //  Assert
+            Assert.Equal(categoryToSelect, result);
+        }
+    }
+}
---
+++ SportsStore.Tests/ProductControllerTests.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SportsStore.Controllers;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
+using Moq;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.Tests
+{
+    public class ProductControllerTests
+    {
+        [Fact]
+        public void Can_Paginate()
+        {
+            //  Arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+                new Product {ProductID = 4, Name = "P4"},
+                new Product {ProductID = 5, Name = "P5"}
+            });
+
+            ProductController controller = new ProductController(mock.Object) { PageSize = 3 };
+
+            //  Act
+            ProductsListViewModel result = controller.List(null, 2).ViewData.Model as ProductsListViewModel;
+
+            //  Assert
+            Product[] prodArray = result.Products.ToArray();
+            Assert.True(prodArray.Length == 2);
+            Assert.Equal("P4", prodArray[0].Name);
+            Assert.Equal("P5", prodArray[1].Name);
+        }
+
+        [Fact]
+        public void Can_Send_Pagination_View_Model()
+        {
+            //  Arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1"},
+                new Product { ProductID = 2, Name = "P2"},
+                new Product { ProductID = 3, Name = "P3"},
+                new Product { ProductID = 4, Name = "P4"},
+                new Product { ProductID = 5, Name = "P5"}
+            });
+
+            ProductController controller = new ProductController(mock.Object) { PageSize = 3 };
+
+            //  Act
+            ProductsListViewModel result = controller.List(null, 2).ViewData.Model as ProductsListViewModel;
+
+            //  Assert
+            PagingInfo pageInfo = result.PagingInfo;
+            Assert.Equal(2, pageInfo.CurrentPage);
+            Assert.Equal(3, pageInfo.ItemsPerPage);
+            Assert.Equal(5, pageInfo.TotalItems);
+            Assert.Equal(2, pageInfo.TotalPages);
+        }
+
+        [Fact]
+        public void Can_Filter_Products()
+        {
+            //  Arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1", Category = "Cat1"},
+                new Product {ProductID = 2, Name = "P2", Category = "Cat2"},
+                new Product {ProductID = 3, Name = "P3", Category = "Cat1"},
+                new Product {ProductID = 4, Name = "P4", Category = "Cat2"},
+                new Product {ProductID = 5, Name = "P5", Category = "Cat3"}
+            });
+
+            ProductController controller = new ProductController(mock.Object) { PageSize = 3 };
+
+            //  Act
+            var result = controller.List("Cat1", 1).ViewData.Model as ProductsListViewModel;
+
+            //  Assert
+            Product[] prodArray = result.Products.ToArray();
+            Assert.True(prodArray.Length == 2);
+            Assert.True(prodArray[0].Name == "P1" && prodArray[0].Category == "Cat1");
+            Assert.True(prodArray[1].Name == "P3" && prodArray[0].Category == "Cat1");
+        }
+
+        [Fact]
+        public void Generate_Category_Specific_Product_Count()
+        {
+            //  Arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1", Category = "Cat1"},
+                new Product {ProductID = 2, Name = "P2", Category = "Cat2"},
+                new Product {ProductID = 3, Name = "P3", Category = "Cat1"},
+                new Product {ProductID = 4, Name = "P4", Category = "Cat2"},
+                new Product {ProductID = 5, Name = "P5", Category = "Cat3"}
+            });
+
+            ProductController controller = new ProductController(mock.Object) { PageSize = 3 };
+
+            Func<ViewResult, ProductsListViewModel> GetModel = result => result?.ViewData?.Model as ProductsListViewModel;
+
+            //  Act
+            int? res1 = GetModel(controller.List("Cat1"))?.PagingInfo.TotalItems;
+            int? res2 = GetModel(controller.List("Cat2"))?.PagingInfo.TotalItems;
+            int? res3 = GetModel(controller.List("Cat3"))?.PagingInfo.TotalItems;
+            int? resAll = GetModel(controller.List(null))?.PagingInfo.TotalItems;
+
+            //  Assert
+            Assert.Equal(2, res1);
+            Assert.Equal(2, res2);
+            Assert.Equal(1, res3);
+            Assert.Equal(5, resAll);
+        }
+    }
+}
